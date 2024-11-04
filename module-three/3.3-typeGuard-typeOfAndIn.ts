862faad0e53df4e2f36df612dce1f278c7b74761
Tareq <razzbabut@gmail.com>
2024-11-04 16:46:13 +0600

    complete type guard with typeof and in

---
+++ module-three/3.3-typeGuard-typeOfAndIn.ts
@@ -0,0 +1,52 @@
+{
+
+    // type guard
+
+    // type guard --> typeof
+
+    type Alphanumeric = string | number;
+
+    const add = (param1: Alphanumeric, param2: Alphanumeric): Alphanumeric => {
+        if (typeof param1 === "number" && param2 === "number") {
+            return param1 + param2
+        } else {
+            return param1.toString() + param2.toString();
+        }
+    }
+
+    const result = add(4, "1")
+    console.log(result)
+
+
+    // type in
+
+    type NormalUser = {
+        name: string;
+    }
+    type AdminUser = {
+        name: string;
+        role: string;
+    }
+
+    const getUser = (user: NormalUser | AdminUser) => {
+        if ("role" in user) {
+            return console.log(`My name is ${user.name} and my role ${user.role}`)
+        } else {
+            return console.log(`My name is ${user.name}`)
+        }
+    }
+
+    const normalUser : NormalUser = {
+        name: "Mr. normal man"
+    }
+    const adminUser : AdminUser = {
+        name: "Mr. normal man",
+        role: "Admin"
+    }
+
+    getUser(normalUser)
+
+
+
+    
+}
